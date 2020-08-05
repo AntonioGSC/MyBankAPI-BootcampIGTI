d2c59414fd593c28dff1e355c60e86665e217947
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-08-05 16:34:54 -0300

    put-and-path

--- routes/accounts.js
+++ routes/accounts.js
@@ -50,4 +50,50 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = await readFile(global.fileName);
+    const json = JSON.parse(data);
+    json.accounts = json.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(json, null, 2));
+    res.send('Conta Removida com sucesso');
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    data.accounts[index] = account;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.patch('/updateBalance', async (req, res) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    data.accounts[index].balance = account.balance;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.send(data.accounts[index]);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
 export default router;

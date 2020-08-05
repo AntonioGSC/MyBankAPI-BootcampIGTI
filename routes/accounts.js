4533bc07494a73625a66bfe879e1881491082794
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-08-05 14:36:18 -0300

    Post-Gets

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,4 +1,53 @@
 import express from 'express';
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = fs;
+
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+  try {
+    let account = req.body;
+    const data = await readFile(global.fileName);
+    const json = JSON.parse(data);
+
+    account = {
+      id: json.nextID++,
+      ...account,
+    };
+
+    json.accounts.push(account);
+
+    await writeFile(global.fileName, JSON.stringify(json, null, 2));
+
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const data = await readFile(global.fileName);
+    const json = JSON.parse(data);
+    delete json.nextID;
+    res.send(json);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const data = await readFile(global.fileName);
+    const json = JSON.parse(data);
+    const account = json.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
 
 export default router;
